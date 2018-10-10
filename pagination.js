6ab0a99006a380a5c81c088eb06e0c8f8bf60a22
ver4.nata@gmail.com <ver4.nata@gmail.com>
2018-10-10 17:24:43 +0300

    make sort pagination

--- pagination.js
+++ pagination.js
@@ -1,1 +1,17 @@
+import users from './users';
+function Query(data) {
+    this.data = data;
+    this.state = [];
+ 
+}
+Query.prototype.pagination = function(limit, page) {
+    let newArr = [];
+    let start =limit * page - limit;
+    let end = limit * page;
+    newArr = this.data.slice(start, end).map(user => user.name);
+    this.state = newArr;
+}
+let queryUsers = new Query(users);
 
+queryUsers.pagination(2,3);
+console.log(queryUsers.state);
